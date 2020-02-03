62a482970161dbf0458d0b7d561cd5bbb882a83e
etcadinfinitum <lizzy.presland@gmail.com>
2020-02-02 16:23:12 -0800

    Rename test files (alphabetization)

--- test/graph/run.sh
+++ test/graph/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-g++ -o test -I../../src/graph -I. ../../src/graph/Vertex.cpp vertex_tests.cpp
+g++ -o test -I../../src/graph -I. ../../src/graph/Vertex.cpp test_vertex.cpp
 
 ./test
 valgrind ./test
 rm test
 
-g++ -o test -I../../src/graph -I. ../../src/graph/Graph.cpp ../../src/graph/Edge.cpp ../../src/graph/Vertex.cpp graph_tests.cpp 
+g++ -o test -I../../src/graph -I. ../../src/graph/Edge.cpp ../../src/graph/Vertex.cpp test_edge.cpp
+./test
+valgrind ./test
+rm test
+
+g++ -o test -I../../src/graph -I. ../../src/graph/Graph.cpp ../../src/graph/Edge.cpp ../../src/graph/Vertex.cpp test_graph.cpp 
 
 ./test
 valgrind ./test
