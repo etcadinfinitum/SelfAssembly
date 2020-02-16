b9b68f6f2c3a6fa528aafbc996719d9a9a5fcad1
etcadinfinitum <lizzy.presland@gmail.com>
2020-02-16 14:00:05 -0800

    Implement splitTree() function (now with tests!)

--- test/graph/test_graph.cpp
+++ test/graph/test_graph.cpp
@@ -11,6 +11,7 @@ bool createNonEmptyGraphFromFile();
 bool createNonEmptyGraphFromBoutiqueVertices();
 bool testCopyAssignment();
 bool testCopyConstructor();
+bool testTreeSplitting();
 
 int main() {
     assert(createEmptyGraph());
@@ -18,6 +19,7 @@ int main() {
     assert(createNonEmptyGraphFromBoutiqueVertices());
     assert(testCopyAssignment());
     assert(testCopyConstructor());
+    assert(testTreeSplitting());
     return 0;
 }
 
@@ -77,5 +79,43 @@ bool testCopyConstructor() {
     cout << "Graph g: \n" << g;
     cout << "Graph h: \n" << h;
     cout << endl;
+    return true;
+}
+
+bool testTreeSplitting() {
+    cout << "Testing tree splitting along existing edge...\n";
+    Graph g;
+    shared_ptr<Vertex> v1 = make_shared<Vertex>("a");
+    shared_ptr<Vertex> v2 = make_shared<Vertex>("b");
+    shared_ptr<Vertex> v3 = make_shared<Vertex>("c");
+    shared_ptr<Vertex> v4 = make_shared<Vertex>("d");
+    assert(g.join(v1, v2));
+    assert(g.join(v2, v3));
+    assert(g.join(v3, v4));
+    shared_ptr<Edge> eSuccess = make_shared<Edge>(v2, v3);
+    pair<Graph*, Graph*>* split1 = g.splitTree(eSuccess);
+    assert(split1 != nullptr);
+    assert(split1->first != nullptr);
+    assert(split1->second != nullptr);
+    cout << "G1 half of tree split: \n" << *(split1->first);
+    cout << "G2 half of tree split: \n" << *(split1->second);
+    assert(split1->first->containsVertex(v1));
+    assert(split1->first->containsVertex(v2));
+    assert(split1->first->containsEdge(make_shared<Edge>(v1, v2)));
+    assert(!split1->first->containsVertex(v3));
+    assert(!split1->first->containsVertex(v4));
+    assert(!split1->first->containsEdge(eSuccess));
+    assert(!split1->first->containsEdge(make_shared<Edge>(v3, v4)));
+    assert(split1->second->containsVertex(v3));
+    assert(split1->second->containsVertex(v4));
+    assert(split1->second->containsEdge(make_shared<Edge>(v3, v4)));
+    assert(!split1->second->containsVertex(v1));
+    assert(!split1->second->containsVertex(v2));
+    assert(!split1->second->containsEdge(eSuccess));
+    assert(!split1->second->containsEdge(make_shared<Edge>(v1, v2)));
+    cout << endl;
+    delete split1->first;
+    delete split1->second;
+    delete split1;
     return true;
 }
