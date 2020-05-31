39b2676f499588b041f007e52a39ec7fef71f494
Lizzy Presland <lizzy.presland@gmail.com>
2020-05-30 17:23:56 -0700

    Implement .ino file writing after rule parsing

--- src/device_adaption/arduino/generator.py
+++ src/device_adaption/arduino/generator.py
@@ -1,17 +1,39 @@
 import static
 import sys
+import os
+from RuleParser import *
 
 def write_ruleset(rule):
-    return '    { pairs: { { older: ' + label + ', newer: ' + label + '}, { older: ' + label + ', newer: ' + label + '}, }, },'
+    # TODO: support labels of any data type with proper quotations and string escaping
+    return '    { pairs: { { older: %s, newer: %s }, { older: %s, newer: %s }, }, },\n' % (rule[0][0], rule[0][1], rule[1][0], rule[1][1])
 
-def write_rules():
+def write_rules(rules):
+    if not os.path.exists('output'):
+        os.mkdir('output')
     with open('output/sketch.ino', 'w') as outfile:
+        # write file opening segment
         outfile.write(static.beginning)
 
+        # write out label data
+        outfile.write('const uint8_t MAX_LABELS = %s;\n' % len(rules._labels))
+        outfile.write('uint8_t CURR_LABEL_INDEX = 0;\n')
+        outfile.write('const uint8_t labels[MAX_LABELS] = {\n')
+        for label in sorted(list(rules._labels)):
+            outfile.write('    %s,\n' % label)
+
+        # write rule label pairs
+        outfile.write('const int RULE_COUNT = %s;\n' % len(rules))
+        outfile.write('\nrule ruleset[RULE_COUNT] = {\n')
+        for rule in rules:
+            outfile.write(write_ruleset(list(rule)))
+        outfile.write('};\n')
+
+        # write file remainder
         outfile.write(static.end)
 
 def main():
-    pass
+    rules = RuleParser([line for line in sys.stdin.read().splitlines() if line.strip()])
+    write_rules(rules)
 
 if __name__=='__main__':
     main()
